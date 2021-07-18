d2b48b5f3089f396687377224cea78cfa00aa4fa
Dalaias <dalaiaas1327@gmail.com>
2021-07-18 14:39:28 -0600

    subiendo archivo tarea1 c++

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char** argv){
+   int n1, n2, suma;
+   cout<<"ingrese primer numero: ";
+   cin>>n1;
+   cout<<"ingrese segundo numero: ";
+   cin>>n2;
+   
+   suma = n1 + n2;
+   
+   cout<<"la suma de "<<n1<<" + "<<n2<<" es: "<<suma<<endl;
+   
+   
+   int resta;
+   cout<<"ingrese primer numero: ";
+   cin>>n1;
+   cout<<"ingrese segundo numero: ";
+   cin>>n2;
+   
+   resta = n1 - n2;
+   cout<<"la resta de "<<n1<<" - "<<n2<<" es: "<<resta<<endl;
+   
+   int multiplicacion;
+   cout<<"ingrese primer numero: ";
+   cin>>n1;
+   cout<<"ingrese segundo numero: ";
+   cin>>n2;
+   
+   multiplicacion = n1 * n2;
+   cout<<"la multiplicacion de "<<n1<<" * "<<n2<<" es: "<<multiplicacion<<endl;
+   
+   int division;
+   cout<<"ingrese primer numero: ";
+   cin>>n1;
+   cout<<"ingrese segundo numero: ";
+   cin>>n2;
+   
+   division = n1 / n2;
+   cout<<"la division de "<<n1<<" / "<<n2<<" es: "<<division<<endl;
+
+return 0;
+
+}
+
+   
+   
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
